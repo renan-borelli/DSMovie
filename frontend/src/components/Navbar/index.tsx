654a9b440105d1e0e8d0210c62085011096d43bb
unknown <renan.andrade.ismart@gmail.com>
2022-05-11 20:40:18 -0300

    Routes

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar() {
             <nav className='container'>
                 <div className='dsmovie-nav-content'>
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/renan-borelli" target="_blank" rel="nonreferrer">
+                    <a href="https://github.com/renan-borelli" target="_blank" rel="noreferrer">
                         <div className='dsmovie-contact-container'>
                             <GithubIcon />
                             <p className='dsmovie-contact-link'>/renan-borelli</p>
